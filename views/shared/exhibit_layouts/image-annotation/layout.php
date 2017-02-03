fab38f4245451aa2ef1bb1664c7860069b82de8e
Jim Safley <jimsafley@gmail.com>
2017-02-03 11:50:46 -0500

    Add captions

--- views/shared/exhibit_layouts/image-annotation/layout.php
+++ views/shared/exhibit_layouts/image-annotation/layout.php
@@ -7,16 +7,23 @@ if (isset($options['image-annotation'])) {
 <div class="exhibit-items left fullsize captions-center">
     <?php foreach ($attachments as $attachment): ?>
         <?php
+        // Note, cannot use $this->exhibitAttachment() or file_image() becuase
+        // we need to append a fragment to the image src URL so Annotorious can
+        // uniquely identify the image on the page.
         $file = $attachment->getFile();
-        $uri = $file->hasThumbnail() ? $file->getWebPath('fullsize') : img('fallback-file.png');
-        $id = 'image-annotation-' . $attachment->id;
-        $attributes = array(
-            'src' => $uri . '#' . $id,
-            'id' => $id,
+        $imageUrl = $file->hasThumbnail()
+            ? $file->getWebPath('fullsize')
+            : img('fallback-file.png');
+        $imageId = 'image-annotation-' . $attachment->id;
+        $imageAttributes = array(
+            'src' => $imageUrl . '#' . $imageId,
+            'id' => $imageId,
             'class' => 'image-annotation-image',
             'data-image-annotations' => $annotations,
         );
+        $imageTag = sprintf('<img %s>', tag_attributes($imageAttributes));
+        echo link_to($file, 'show', $imageTag);
+        echo $this->exhibitAttachmentCaption($attachment)
         ?>
-        <img <?php echo tag_attributes($attributes); ?>>
     <?php endforeach; ?>
 </div>

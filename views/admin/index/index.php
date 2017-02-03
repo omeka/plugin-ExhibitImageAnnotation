543323b4465411370b7d0b6727d7789fd8cbebac
Jim Safley <jimsafley@gmail.com>
2017-02-03 13:04:27 -0500

    Refactor for consistency

--- views/admin/index/index.php
+++ views/admin/index/index.php
@@ -1,10 +1,13 @@
 <?php
-$uri = $file->hasThumbnail() ? $file->getWebPath('fullsize') : img('fallback-file.png');
-$id = 'image-annotation-' . $index;
-$attributes = array(
-    'src' => $uri . '#' . $id,
-    'id' => $id,
+// Note, cannot use file_image() becuase we need to append a fragment to the
+// image src URL so Annotorious can uniquely identify the image on the page.
+$imageUrl = $file->hasThumbnail()
+    ? $file->getWebPath('fullsize')
+    : img('fallback-file.png');
+$imageId = 'image-annotation-' . $index;
+$imageAttributes = array(
+    'src' => $imageUrl . '#' . $imageId,
+    'id' => $imageId,
     'class' => 'image-annotation-image',
 );
-?>
-<img <?php echo tag_attributes($attributes); ?>>
+echo sprintf('<img %s>', tag_attributes($imageAttributes));
